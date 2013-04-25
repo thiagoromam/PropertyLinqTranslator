cb62d7b39489f4b925313ea54a7b2f76b5fc9f1e
Thiago Luis Romam Matheus <thiago.romam@outlook.com>
2013-04-25 14:27:44 -0300

    adicionando o projeto ao git

---
+++ PropertyLinqTranslator/PropertyTranslator/CompiledExpression.Generic.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Linq.Expressions;
+
+namespace PropertyTranslator
+{
+    /// <summary>
+    /// Generic extension to the <see cref="CompiledExpression"/> class.
+    /// </summary>
+    /// <typeparam name="T">The object (e.g. entity) type.</typeparam>
+    /// <typeparam name="TResult">Type of the result of the expression.</typeparam>
+    public class CompiledExpression<T, TResult> : CompiledExpression
+    {
+        /// <summary>
+        /// The base expression.
+        /// </summary>
+        private readonly Expression<Func<T, TResult>> expression;
+
+        /// <summary>
+        /// The compiled expression.
+        /// </summary>
+        private readonly Func<T, TResult> function;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CompiledExpression{T, TResult}" /> class.
+        /// </summary>
+        /// <param name="expression">The expression.</param>
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1006:DoNotNestGenericTypesInMemberSignatures", Justification = "Necessary for evaluation of results.")]
+        public CompiledExpression(Expression<Func<T, TResult>> expression)
+        {
+            this.expression = expression;
+            this.function = expression.Compile();
+        }
+
+        /// <summary>
+        /// Gets the undelying lambda expression.
+        /// </summary>
+        /// <value>The lambda expression.</value>
+        internal override LambdaExpression BaseExpression
+        {
+            get { return this.expression; }
+        }
+
+        /// <summary>
+        /// Evaluates the compiled expression on the specified instance.
+        /// </summary>
+        /// <param name="instance">The instance.</param>
+        /// <returns></returns>
+        public TResult Evaluate(T instance)
+        {
+            return this.function(instance);
+        }
+    }
+}
---
+++ PropertyLinqTranslator/PropertyTranslator/CompiledExpressionMap.Generic.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace PropertyTranslator
+{
+    /// <summary>
+    /// Generic extension to the <see cref="CompiledExpressionMap"/>
+    /// </summary>
+    /// <typeparam name="T">The object (e.g. entity) type.</typeparam>
+    /// <typeparam name="TResult">Type of the result of the expression.</typeparam>
+    [DataContract]
+    public class CompiledExpressionMap<T, TResult> : CompiledExpressionMap
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CompiledExpressionMap{T, TResult}" /> class.
+        /// </summary>
+        public CompiledExpressionMap()
+        {
+        }
+        
+        /// <summary>
+        /// Evaluates the compiled expression for current thread ui culture on the specified instance.
+        /// </summary>
+        /// <param name="instance">Object instance to be evaluated.</param>
+        /// <returns></returns>
+        /// <exception cref="System.InvalidOperationException">If no <see cref="CompiledExpression"/> for current environment is available.</exception>
+        public TResult Evaluate(T instance)
+        {
+            return this.GetValue().Evaluate(instance);
+        }
+
+        /// <summary>
+        /// Gets the compiled expression for current thread ui culture.
+        /// </summary>
+        /// <returns></returns>
+        /// <exception cref="System.InvalidOperationException">If no <see cref="CompiledExpression"/> for current envirnoment is available.</exception>
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1024:UsePropertiesWhereAppropriate", Justification = "Throws an exception")]
+        public new CompiledExpression<T, TResult> GetValue()
+        {
+            CompiledExpression result;
+
+            if (this.TryGetValue(out result))
+            {
+                return result as CompiledExpression<T, TResult>;
+            }
+
+            throw new InvalidOperationException("No expression registered for specified method.");
+        }
+
+        /// <summary>
+        /// Tries to get the compiled expression for current thread ui culture.
+        /// </summary>
+        /// <param name="expression">The expression.</param>
+        /// <returns></returns>
+        public bool TryGetValue(out CompiledExpression<T, TResult> expression)
+        {
+            CompiledExpression result;
+            expression = null;
+
+            if (base.TryGetValue(out result))
+            {
+                expression = result as CompiledExpression<T, TResult>;
+            }
+
+            return expression != null;
+        }
+    }
+}
